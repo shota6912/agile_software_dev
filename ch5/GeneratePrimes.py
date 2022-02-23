80f9445f66ab284ada44fd6ce489d2a2dbd81e56
shota69123 <YyxA67eyh4Dt5wv>
2022-02-23 13:01:25 +0900

    v2

--- ch5/GeneratePrimes.py
+++ ch5/GeneratePrimes.py
@@ -19,31 +19,37 @@ class GeneratePrimes:
     def __init__(self):
         pass
 
-    def generatePrimes(self, max_value):
-        if max_value >= 2:
-            s = max_value + 1
-            f = [True for i in range(s)]
-            f[0] = f[1] = False
-
-            for i in range(2, int(math.sqrt(s)) + 1):
-                if f[i]:
-                    for j in range(2 * i, s, i):
-                        f[j] = False
-
-            count = 0
-            for i in range(s):
-                if f[i]:
-                    count += 1
-
-            primes = [0 for i in range(count)]
-
-            j = 0
-            for i in range(s):
-                if f[i]:
-                    primes[j] = i
-                    j += 1
-            return primes
+    def generatePrimes(self, maxValue):
+        if maxValue < 2:
+            return []
 
         else:
-            return []
-        
+            self.initializeSieve(maxValue)
+            self.sieve()
+            self.loadPrimes()
+            return self.primes
+
+    def loadPrimes(self):
+        self.count = 0
+        for i in range(self.s):
+            if self.f[i]:
+                self.count += 1
+
+        self.primes = [0 for i in range(self.count)]      
+        j = 0
+        for i in range(self.s):
+            if self.f[i]:
+                self.primes[j] = i
+                j += 1
+        return self.primes
+
+    def sieve(self):
+        for i in range(2, int(math.sqrt(self.s)) + 1):
+            if self.f[i]:
+                for j in range(2 * i, self.s, i):
+                    self.f[j] = False
+
+    def initializeSieve(self, maxValue):
+        self.s = maxValue + 1
+        self.f = [True for i in range(self.s)]
+        self.f[0] = self.f[1] = False
