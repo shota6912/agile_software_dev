fca034fa1a14f95844806ddda429d82514023ce6
shota69123 <YyxA67eyh4Dt5wv>
2022-02-23 12:37:23 +0900

    v1

--- ch5/GeneratePrimes.py
+++ ch5/GeneratePrimes.py
@@ -1,3 +1,18 @@
+"""
+This class Generates prime numbers up to a suser specified maximum.
+the algorithm used is the Sieve of Eratosthenes.
+
+Erastosthenes of Cyrene, b. c. 276 BC, Cyrene, Libya
+d. c. 194, Alexandria. The first man to calculate the circumference
+of the Earth. Also known for working on calendars with leap
+years and ran the library at Alexandria.
+
+The algorithm is quite simple. Given an array of integers starting
+at 2. Cross out all multiples of 2. Find the next uncrossed 
+integer, and cross out all of its multiples. Repeat until
+you have passed the aquare root of the maximum value.
+"""
+
 import math
 
 class GeneratePrimes:
